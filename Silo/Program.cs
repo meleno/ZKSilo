0f89d4cedf571df541fd4a31a097128f3eeca407
Roberto Vazquez Iglesias <roberto.vazquez@zkteco.local>
2018-10-14 22:29:03 +0200

    Initialization of the solution. Added the silo project and the silo´s configuration project.

---
+++ Silo/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using SiloConfig;
+using System;
+using System.Threading.Tasks;
+
+namespace Silo
+{
+	class Program
+	{
+		public static int Main(string[] args)
+		{
+			return RunMainAsync().Result;
+		}
+
+		private static async Task<int> RunMainAsync()
+		{
+			try
+			{
+				var host = await StartSilo2();
+				Console.WriteLine("Press Enter to terminate...");
+				Console.ReadLine();
+
+				await host.StopAsync();
+
+				return 0;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				return 1;
+			}
+		}
+
+		private static async Task<ISiloHost> StartSilo2()
+		{
+			IConfiguration config = SiloConfigurationProvider.GetSiloConfiguration();
+
+			// define the cluster configuration
+			var builder = new SiloHostBuilder()
+				.UseLocalhostClustering()
+				.Configure<ClusterOptions>(config)
+				.Configure<EndpointOptions>(config)
+				.Configure<AdoNetClusteringSiloOptions>(config)
+				//.ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(HelloGrain).Assembly).WithReferences())
+				.ConfigureLogging(logging =>
+				{
+					logging.AddNLog();
+					logging.AddConsole();
+				});
+
+			var host = builder.Build();
+			await host.StartAsync();
+			return host;
+		}
+	}
+}
---
+++ SiloConfig/SiloConfigurationProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SiloConfig
+{
+	public static class SiloConfigurationProvider
+	{
+		private static IConfiguration m_siloConfiguration;
+
+		public static IConfiguration GetSiloConfiguration()
+		{
+			if (m_siloConfiguration != null)
+			{
+				var configBuilder = new ConfigurationBuilder()
+						  .SetBasePath(Directory.GetCurrentDirectory())
+						  .AddJsonFile("config.json", reloadOnChange: true, optional: false);
+
+				m_siloConfiguration = configBuilder.Build();
+			}
+
+			return m_siloConfiguration;
+		}
+	}
+}
