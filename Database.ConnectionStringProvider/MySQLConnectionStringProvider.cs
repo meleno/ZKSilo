e587e276998b36295d2897ecf796f8eed1417078
meleno <meleno@gmail.com>
2019-05-03 15:25:15 +0200

    Added the necessary code to make the unit test work

--- Database.ConnectionStringProvider/MySQLConnectionStringProvider.cs
+++ Database.ConnectionStringProvider/MySQLConnectionStringProvider.cs
@@ -8,7 +8,7 @@ namespace Database.ConnectionStringProvider
     {
         public string GetConnectionString(DatabaseConfig config)
         {
-            throw new NotImplementedException();
+            return $"Data Source = {config.ServerAddress}; Port ={config.ServerPort}; Database ={config.DatabaseName}; User ID = {config.UserName}; Password ={config.Password}; pooling = true; SslMode ={config.UseSSL}; connection lifetime = 300; Treat Tiny As Boolean = false; character set = utf8; AllowPublicKeyRetrieval = True; ";
         }
     }
 }
