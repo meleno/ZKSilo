af1e0adb8619847aa0c999e35ce691cae7b51851
meleno <meleno@gmail.com>
2019-05-03 23:39:28 +0200

    Generated the projects for unit testing and generation of the main database.

---
+++ Database.IDb.DatabaseGeneratorMain/SQLServerDatabaseGeneratorMain.cs
@@ -0,0 +1,13 @@
+﻿using Database.IDatabase;
+using Database.IDb.DatabaseGenerator;
+
+namespace Database.IDb.DatabaseGeneratorMain
+{
+	public class SQLServerDatabaseGeneratorMain : GeneralDatabaseGenerator
+	{
+		public SQLServerDatabaseGeneratorMain(IDbConnectionFactory connectionProvider) : base(connectionProvider)
+		{ }
+
+		protected override string Path => $"Scripts\\Main\\SQLServer";
+	}
+}
